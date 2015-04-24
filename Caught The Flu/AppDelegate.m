0ac826f7b71ccb418b016177f65bdac2ca5b74f0
Matt Schulte <schultetwin1@gmail.com>
2015-04-24 08:07:47 -0400

    Basic working version

--- Caught The Flu/AppDelegate.m
+++ Caught The Flu/AppDelegate.m
@@ -8,6 +8,8 @@
 
 #import "AppDelegate.h"
 
+#import "LibInfected/LibInfected.h"
+
 @interface AppDelegate ()
 
 @end
@@ -17,6 +19,8 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
+    
+    [LibInfected sharedLibInfected];
     return YES;
 }
 
--- Caught The Flu/ViewController.m
+++ Caught The Flu/ViewController.m
@@ -8,7 +8,12 @@
 
 #import "ViewController.h"
 
-@interface ViewController ()
+#import "LibInfected/LibInfected.h"
+
+@interface ViewController () <LibInfectedDelgate>
+@property (weak, nonatomic) IBOutlet UISegmentedControl *healthyInfectedSegment;
+
+@property BOOL haveFlu;
 
 @end
 
@@ -17,11 +22,51 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    
+    if (![[LibInfected sharedLibInfected] running]) {
+        [(LibInfected*)[LibInfected sharedLibInfected] start];
+    }
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+- (IBAction)healthyInfectedChanged:(id)sender {
+    if (self.healthyInfectedSegment.selectedSegmentIndex == 2) {
+        self.haveFlu = YES;
+    } else {
+        self.haveFlu = NO;
+    }
+}
+
+#pragma mark - LibInfectedDelegate
+
+-(void)bleOff {
+    
+}
+-(void)bleOn {
+    
+}
+
+-(CBATTRequest*)receivedReadRequest:(CBATTRequest*) request; {
+    const char data[2] = {0,1};
+    if (self.haveFlu) {
+        request.value = [[NSData alloc] initWithBytes:&data[1] length:1];
+    } else {
+        request.value = [[NSData alloc] initWithBytes:&data[0] length:1];
+    }
+    return request;
+}
+
+-(void) receivedReadResponse:(NSData*) data peripheralRSSI:(NSNumber*) rssi {
+    char recvdata[1];
+    [data getBytes:recvdata length:1];
+    
+    if (recvdata[0] == 1) {
+        self.haveFlu = YES;
+    }
 }
 
 @end
