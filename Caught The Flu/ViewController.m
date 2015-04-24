c733e9ba92ec594dc9ff9dab60e0f761c703272e
Matt Schulte <schultetwin1@gmail.com>
2015-04-24 10:11:30 -0400

    Created a basic app to warn users if they may have caught the Flu

--- Caught The Flu/ViewController.m
+++ Caught The Flu/ViewController.m
@@ -12,20 +12,34 @@
 
 @interface ViewController () <LibInfectedDelgate>
 @property (weak, nonatomic) IBOutlet UISegmentedControl *healthyInfectedSegment;
+@property (weak, nonatomic) IBOutlet UILabel *exposedLabel;
 
 @property BOOL haveFlu;
+@property NSDate* lastExposure;
 
 @end
 
 @implementation ViewController
+- (IBAction)feelingChanged:(id)sender {
+    if (self.healthyInfectedSegment.selectedSegmentIndex == 1) {
+        self.haveFlu = YES;
+    } else {
+        self.haveFlu = NO;
+    }
+}
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    ((LibInfected*)[LibInfected sharedLibInfected]).delegate = self;
+    self.lastExposure = nil;
+    [self.exposedLabel setText:@"NO"];
     // Do any additional setup after loading the view, typically from a nib.
     
-    if (![[LibInfected sharedLibInfected] running]) {
-        [(LibInfected*)[LibInfected sharedLibInfected] start];
-    }
+    UIUserNotificationType types = UIUserNotificationTypeAlert;
+    
+    UIUserNotificationSettings *mySettings = [UIUserNotificationSettings settingsForTypes:types categories:nil];
+    
+    [[UIApplication sharedApplication] registerUserNotificationSettings:mySettings];
 }
 
 - (void)didReceiveMemoryWarning {
@@ -33,21 +47,13 @@
     // Dispose of any resources that can be recreated.
 }
 
-- (IBAction)healthyInfectedChanged:(id)sender {
-    if (self.healthyInfectedSegment.selectedSegmentIndex == 2) {
-        self.haveFlu = YES;
-    } else {
-        self.haveFlu = NO;
-    }
-}
-
 #pragma mark - LibInfectedDelegate
 
 -(void)bleOff {
-    
+    [(LibInfected*)[LibInfected sharedLibInfected] stop];
 }
 -(void)bleOn {
-    
+    [(LibInfected*)[LibInfected sharedLibInfected] start];
 }
 
 -(CBATTRequest*)receivedReadRequest:(CBATTRequest*) request; {
@@ -64,8 +70,25 @@
     char recvdata[1];
     [data getBytes:recvdata length:1];
     
-    if (recvdata[0] == 1) {
+    if (recvdata[0] == 1 && !self.haveFlu) {
         self.haveFlu = YES;
+        
+        self.lastExposure = [[NSDate alloc] init];
+        
+        NSDateFormatter *formatter = [[NSDateFormatter alloc] init];
+        [formatter setDateFormat:@"EEE MMM dd, yyyy @ hh:mm a"];
+        
+        [self.exposedLabel setText:[formatter stringFromDate:self.lastExposure]];
+        
+        UILocalNotification *notification = [[UILocalNotification alloc] init];
+        
+        // Fire in Flu incubation period
+        NSDate* incubationDate = [[NSDate alloc] initWithTimeIntervalSinceNow:15];
+        notification.alertBody = @"You were infected 3 days ago. Do you have the flu?";
+        [notification setAlertTitle:@"Caught the Flu?"];
+        [notification setFireDate:incubationDate];
+        
+        [[UIApplication sharedApplication] scheduleLocalNotification:notification];
     }
 }
 
